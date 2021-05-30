84a57eceabd4cdab7157fcaf1f2ae296c0a4d960
Luis Guisado <lguisadom@gmail.com>
2021-05-30 12:27:01 -0500

    Componentes en angular

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Andrés', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', createAt: '2017-12-11'},
+  {id: 2, nombre: 'Luis', apellido: 'Guisado', email: 'luis@bolsadeideas.com', createAt: '2017-12-12'},
+  {id: 3, nombre: 'Jorge', apellido: 'Mena', email: 'jorge@bolsadeideas.com', createAt: '2017-12-13'},
+  {id: 4, nombre: 'James', apellido: 'Zamora', email: 'james@bolsadeideas.com', createAt: '2017-12-14'},
+  {id: 5, nombre: 'Kevin', apellido: 'Chavez', email: 'kevin@bolsadeideas.com', createAt: '2017-12-15'},
+  {id: 6, nombre: 'Jasmin', apellido: 'Navarro', email: 'jasmin@bolsadeideas.com', createAt: '2017-12-16'}
+];
