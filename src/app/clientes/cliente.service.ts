23a545256ea3877dce23737b05881d6db4655d5f
Luis Guisado <lguisadom@gmail.com>
2021-06-15 20:53:29 -0500

    Cargando los datos en el formulario para actualizar

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -43,4 +43,8 @@ export class ClienteService {
   create(cliente: Cliente): Observable<Cliente> {
     return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders});
   }
+
+  getCliente(id: Number): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+  }
 }
